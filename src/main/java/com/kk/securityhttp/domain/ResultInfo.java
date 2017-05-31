7a14e98b49e75d7faaf1c4baf2bdc349e036203f
wuhanfeiyou <wuhanfeiyou@gmail.com>
2017-05-31 09:47:35 +0800

    test meger

--- src/main/java/com/kk/securityhttp/domain/ResultInfo.java
+++ src/main/java/com/kk/securityhttp/domain/ResultInfo.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.annotation.JSONField;
 
 public class ResultInfo<T> {
     public int code;
+	
     @JSONField(name = "msg")
     public String message;
     public T data;
